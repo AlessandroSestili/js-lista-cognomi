f0578f18150482fbeb41483b237d243ac9f9ca0f
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-13 23:48:44 +0200

    finisco progetto - mi manca solo di mettere la prima lettera di ogni cognome in upper case

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-var listaCognomi = ["Bianchi" , "Neri" , "Rossi" , "Verdi" , "Gialli"];
-var cognomeUtente = prompt("Inserisci il tuo cognome");
-listaCognomi.push(cognomeUtente)
+//dichiaro array con tutti i cognomi
+var listaCognomi = ["Stotani" , "Aslam" , "Alessio" , "Marini" , "pontillo" , "CapelliMerda"];
+
+
+// chiedo all'utente il suo cognome in un prompt 
+var cognomeUtente = (prompt("Inserisci il tuo cognome")).toLowerCase();
+
+// pusho il cognome nell'Array lista cognomi 
+listaCognomi.push(cognomeUtente);
 
 
 if(isNaN(cognomeUtente)) {
     
     console.log("Il cognome inserito dall'utente è: " + cognomeUtente)
 
-    // ordina alfabeticamente l'Array listaCognomi 
-    var listaCognomiOrdinata = listaCognomi.sort();
-    
+    // crea un ciclo che prende ogni cognome all'interno dell' Array 
+    // e modifica to lower case e lo mette in una nuovo Array
+    var listaCognomiToLowerCase = [];
+
+    for (var j = 0; j < listaCognomi.length; j++) {
+
+        listaCognomiToLowerCase.push(listaCognomi[j].toLowerCase());
+    }
+
     // stampo nella console la lista aggiornata con cognomeUtente ordinata alfabeticamente 
-    console.log("Ecco la lista di tutti i cognomi ordinata alfabeticamente: " + listaCognomiOrdinata);
-    
-    var posizioneCognome;
+    console.log("Ecco la lista di tutti i cognomi ordinata alfabeticamente: " + listaCognomiToLowerCase.sort()  );
     
     // ciclo finchè non trovo la posizione 
-    for(var i = 0; i < listaCognomiOrdinata.length; i++) {
-        if(cognomeUtente === listaCognomiOrdinata[i]) {
+    var posizioneCognome;
+    for(var i = 0; i < listaCognomiToLowerCase.length; i++) {
+        if(cognomeUtente === listaCognomiToLowerCase[i]) {
             posizioneCognome = i+1;
         }
     }
     
     // stampo nella console la posizione di dove si trova il cognomeUtente
     console.log("Il tuo cognome si trova nella posizione numero " + posizioneCognome)
-
+    
 } else {
-
+    
     alert("Inserisci il tuo cognome. Non un numero!")
 }
+
+
+
+
+
 
 
 
